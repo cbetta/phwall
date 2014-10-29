d1e047588be60130a824af28507854a6795198f1
Cristiano Betta <cbetta@gmail.com>
2014-10-29 17:45:15 +0000

    :Adds auto-updating

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,15 @@
 class AppointmentsController < ApplicationController
+  before_filter :load_startups
+
   def index
+
+  end
+
+  def table
+    render :index, layout: false
+  end
+
+  def load_startups
     startups = Doodle.new(ENV['DATA_URL']).read
     startups = startups.each_slice(startups.length/2).to_a
     @startups = startups[0].zip(startups[1]).flatten
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/table", to: "appointments#table"
   root 'appointments#index'
 end
